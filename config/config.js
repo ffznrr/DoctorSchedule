f4ca603a59181ac2d429f30328fc8832b95f9371
ffznrr <ffznrr@gmail.com>
2025-05-13 15:44:07 +0700

    initializing db

---
+++ config/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    dialect: process.env.DB_DIALECT_DEV,
+  },
+};
